c535348dde6fa22a595370056018700acf315ae5
meeces2911 <462278+meeces2911@users.noreply.github.com>
2025-03-24 17:40:44 +1300

    Version bump to 1.1.1

--- load.py
+++ load.py
@@ -30,7 +30,7 @@ from auth import Auth, SPREADSHEET_ID
 plugin_name = Path(__file__).resolve().parent.name
 logger = logging.getLogger(f'{appname}.{plugin_name}')
 
-VERSION = '1.1.0'
+VERSION = '1.1.1'
 _CAPI_RESPONSE_TK_EVENT_NAME = '<<CAPIResponse>>'
 
 KILLSWITCH_CMDR_UPDATE = 'cmdr info update'
