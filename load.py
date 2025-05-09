e97a396d6fb7ca0aedaedbdf12a19bb6d65cc512
meeces2911 <462278+meeces2911@users.noreply.github.com>
2025-05-10 02:07:05 +1200

    Bump to 1.3.0-beta2

--- load.py
+++ load.py
@@ -34,7 +34,7 @@ from auth import Auth, SPREADSHEET_ID
 plugin_name = Path(__file__).resolve().parent.name
 logger = logging.getLogger(f'{appname}.{plugin_name}')
 
-VERSION = '1.3.0-beta1'
+VERSION = '1.3.0-beta2'
 _CAPI_RESPONSE_TK_EVENT_NAME = '<<CAPIResponse>>'
 
 KILLSWITCH_CMDR_UPDATE = 'cmdr info update'
